dcef6b2cd419a7971f396fe238958e0c57716568
manavrgs92 <manuel.vargas.garrido@outlook.com>
2022-08-16 10:18:34 -0500

    Variables

---
+++ variables.js
@@ -0,0 +1,29 @@
+/** Variables **/
+// Declaración
+let nombre = 'Juanito';
+nombre = 'Paco';
+
+let usuario; // declara una variable
+usuario = 'pollito02'; // inicializar una variable
+
+const apellido = 'Martínez'; // las constantes no se peden reinicializar
+//apellido = 'Pech';
+
+// Concatenación
+let suma = 2 + 2; // suma
+console.log(suma);
+
+let concatStrings = 'Hola ' + 'Manuel'; // concatenación
+console.log(concatStrings);
+let nombreCompleto = nombre + ' ' + apellido; // concatenación
+console.log(nombreCompleto);
+
+// Tipos de variables
+let name = 'Manuel'; // string
+let lastName = 'Vargas'; // string
+let userName = 'manuVar01'; // string
+let edad = 99; // number
+let email = 'manuel.vargas@mymail.abc' // string
+let isOfLegalAge = true; // boolean
+let moneySavings = 10000; // number
+let debts = 100; // number
