3a50ebc8cbf1411ba53df42a6c75f5895585ea46
manavrgs92 <manuel.vargas.garrido@outlook.com>
2022-08-19 20:08:49 -0500

    Leyendo HTML desde JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+/** Conectar JavaScript con HTML **/
 console.log('Hello, world!!');
+
+/** Leyendo HTML desde JavaScript **/
+const h1 = document.querySelector('h1');
+// const h1 = document.getElementById('h1');
+const p = document.querySelector('p');
+const parrafito = document.querySelector('.parrafito');
+const parrafId = document.querySelector('#parrafId');
+const input = document.querySelector('input');
+
+console.log(h1);
+console.log(h1.value);
+
+console.log({
+    h1,
+    p,
+    parrafito,
+    parrafId,
+    input
+});
